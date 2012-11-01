51001470ff60a3751bf751592c4153e43f8016bb
KashiwaShota <nike07th.kc@gmail.com>
2012-11-01 17:18:59 +0900

    first commit

---
+++ src/testApp.cpp
@@ -0,0 +1,89 @@
+#include "testApp.h"
+
+ofVideoGrabber vidGrabber;
+ofImage img;
+int camWidth, camHeight;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	ofBackground(0,0,0);//RGB背景色
+	ofEnableSmoothing();//スムージング開始
+	ofSetFrameRate(10);//フレームレート設定
+	//キャプチャする画像のサイズを指定
+	camWidth = 450;//キャプチャ画面の幅
+	camHeight = 300;//キャプチャ画面の高さ
+	vidGrabber.setVerbose(true);//使用可能のデバイスを探す
+	vidGrabber.initGrabber(camWidth, camHeight);//インターフェイスをデバックに役立つ
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	//キャプチャする
+	vidGrabber.grabFrame();//sequence grabbeから新しいデータを得る為に定期的に呼び出す
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetColor(0xFFFFFF);//描画色(黒)
+	vidGrabber.draw(20,20);//20,20から描画する
+	
+	//現在のフレームのピクセル配列,RGB[width*height*3]のポインタを返します。
+	unsigned char * pixels = vidGrabber.getPixels();
+	//色反転処理
+	for(int i = 0; i < camHeight; i++){
+		for(int j = 0; j < camWidth; j++){
+			int p = i * camWidth * 3 + j * 3;
+			pixels[p] = 255-pixels[p];
+			pixels[p + 1] = 255-pixels[p + 1];
+			pixels[p + 2] = 255-pixels[p + 2];
+		}
+	}
+	//処理後の画像の描画
+	img.setFromPixels(pixels, camWidth, camHeight, OF_IMAGE_COLOR);
+	img.draw(20, 330);//20,330から画像処理したモノを描画する
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
